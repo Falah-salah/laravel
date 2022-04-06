e4b833e1926aa6a7ff0ee5c0219f5840a6ad4d4c
falah-salah <falahsalah959@gmail.com>
2022-04-06 21:55:03 +0300

    apload

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div style="background-color:lightyellow; width:40%; float:left">
+        <h1 style=" width: 40%;">Falah Salah</h1>
+        <img src="IMG_2450.JPG" style="width: 200px; border-radius:50px; height:300px ">
+        <br><br>
+        <pre style="font-size: 16px;">
+Age = 22
+experience = No experience
+degree = bachelor to be
+a third stage IT Student who still does not know what to do
+
+        </pre>
+        <h1> go back to <a href="/ "> Login</a> page</h1>
+
+    </div>
+
+</body>
+
+</html>
